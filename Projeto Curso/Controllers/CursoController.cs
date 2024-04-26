44773ab275ba3b5d737ae23bec8b7a6454a2b7ec
Mariana Morales <contato.marianamoraless@gmail.com>
2024-04-26 19:45:41 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto Curso/Controllers/CursoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleCurso.Controllers
+{
+    public class CursoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();  
+        }
+    }
+}
---
+++ Projeto Curso/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ControleCurso.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
